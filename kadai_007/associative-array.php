7bb4d9d0c86e6f7176e81db95dc55c38604ce2a0
mico777 <141799745+mico777@users.noreply.github.com>
2023-12-19 22:36:58 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>kadai_007</title>
+    <meta charset="UTF-8">
+  </head>
+
+  <body>
+    <p>
+      <?php
+      //連想配列の値を入力
+      $goods_data = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+      //連想配列を出力
+      print_r($goods_data);
+      ?>
+    </p>
+  </body>
+</html>
